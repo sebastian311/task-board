36d368088c36ed8930c31264466dff98d574905f
Sebastian <naesebastian96@gmail.com>
2024-12-01 19:40:59 +0200

    Removed boiler-plate nx component

--- apps/TaskFlowDashboard/src/app/app.routes.ts
+++ apps/TaskFlowDashboard/src/app/app.routes.ts
@@ -1,9 +1,8 @@
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
-  {
-    path: '',
-    component: NxWelcomeComponent,
-  },
+  // {
+  //   path: '',
+  //   component: NxWelcomeComponent,
+  // },
 ];
