9f82959c501f571bc35daf497094410d0a5e7ebb
Sebastian <naesebastian96@gmail.com>
2025-01-05 10:09:30 +0200

    Removed nx welcome component on dashboard

--- apps/FlowChartDashboard/src/app/app.routes.ts
+++ apps/FlowChartDashboard/src/app/app.routes.ts
@@ -1,4 +1,3 @@
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
@@ -11,9 +10,5 @@ export const appRoutes: Route[] = [
     path: 'Authentication',
     loadChildren: () =>
       import('Authentication/Routes').then((m) => m!.remoteRoutes),
-  },
-  {
-    path: '',
-    component: NxWelcomeComponent,
-  },
+  }
 ];
