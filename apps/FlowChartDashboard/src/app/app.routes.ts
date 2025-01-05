825ec864f4975a99f8e97453fe495efdf06eb9d2
Sebastian <naesebastian96@gmail.com>
2025-01-05 11:22:07 +0200

    added basic dashboard shell with routing

--- apps/FlowChartDashboard/src/app/app.routes.ts
+++ apps/FlowChartDashboard/src/app/app.routes.ts
@@ -1,14 +1,26 @@
 import { Route } from '@angular/router';
+import { DashboardShellComponent } from './feature-components/dashboard-component/dashboard-component.component';
 
 export const appRoutes: Route[] = [
   {
-    path: 'Backoffice',
-    loadChildren: () =>
-      import('Backoffice/Routes').then((m) => m!.remoteRoutes),
+    path: '',
+    component: DashboardShellComponent, // Home
+    children: [
+      {
+        path: 'Backoffice',
+        loadChildren: () =>
+          import('Backoffice/Routes').then((m) => m!.remoteRoutes),
+      },
+      {
+        path: 'Authentication',
+        loadChildren: () =>
+          import('Authentication/Routes').then((m) => m!.remoteRoutes),
+      }
+    ]
   },
   {
-    path: 'Authentication',
-    loadChildren: () =>
-      import('Authentication/Routes').then((m) => m!.remoteRoutes),
+    // For invalid paths, redirect to DashboardShellComponent
+    path: '**',
+    redirectTo: ''
   }
 ];
